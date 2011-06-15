8d946e3a9f008bd11404d279525760815ae2ec05
Pawel Lorek <plorek@neubloc.com>
2011-06-16 00:15:59 +0200

    Initial commit

---
+++ javascripts/runner.js
@@ -0,0 +1,16 @@
+PerformanceJS = {};
+(function(){
+  var tests = [];
+  PerformanceJS.addTestCase = function(name, tests){
+    tests.push({name: tests});
+  };
+  var tuneTests = function(){
+    
+  };
+
+  runTests = function(){
+    
+  };
+
+  PerformanceJS.runTests = runTests;
+}());
