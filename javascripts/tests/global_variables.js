2d9c4cf23d2facfd393d015681f1a9c5c24dc40d
Pawel Lorek <plorek@neubloc.com>
2011-09-09 00:04:37 +0200

    Fixing globals speed test after refactor

--- javascripts/tests/global_variables.js
+++ javascripts/tests/global_variables.js
@@ -1,64 +1,64 @@
-PerfJS.addTestCase("Global variables", [
-{
-  name: "One additional global variables",
-  test: function(){
-    i ++; 
-  },
-  setUp: function(){
-    i = 0;
-  },
+(function(){
+  var suite = PerfJS.suite("Global variables");
+  suite.add({
+    name: "One additional global variables",
+    test: function(){
+      i ++; 
+    },
+    setUp: function(){
+      i = 0;
+    },
+    tearDown: function(){
+      delete i;
+    }
+  });
 
-  tearDown: function(){
-    delete i;
-  }
-},
-
-{
-  name: "hundret additional global variables",
-  test: function(){
-    ff ++; 
-  },
-  setUp: function(){
-    var str = "abcdefghijklmnopqrstuvxyz", i, j;
-    for (i = 0; i < 10; i++){
-      for (j = 0; j < 10; j++){
-        window[str[i]+str[j]] = 42;
+  suite.add({
+    name: "hundret additional global variables",
+    test: function(){
+      ff ++; 
+    },
+    setUp: function(){
+      var str = "abcdefghijklmnopqrstuvxyz", i, j;
+      for (i = 0; i < 10; i++){
+        for (j = 0; j < 10; j++){
+          window[str[i]+str[j]] = 42;
+        }
+      }
+    },
+    tearDown: function(){
+      var str = "abcdefghijklmnopqrstuvxyz", i, j;
+      for (i = 0; i < 10; i++){
+        for (j = 0; j < 10; j++){
+          delete window[str[i]+str[j]];
+        }
       }
     }
-  },
+  });
 
-  tearDown: function(){
-    var str = "abcdefghijklmnopqrstuvxyz", i, j;
-    for (i = 0; i < 10; i++){
-      for (j = 0; j < 10; j++){
-        delete window[str[i]+str[j]];
+  suite.add({
+    name: "625 additional global variables",
+    test: function(){
+      ll ++; 
+    },
+    setUp: function(){
+      var str = "abcdefghijklmnopqrstuvxyz", i, j;
+      for (i = 0; i < str.length; i++){
+        for (j = 0; j < str.length; j++){
+          window[str[i]+str[j]] = 42;
+        }
+      }
+    },
+    tearDown: function(){
+      var str = "abcdefghijklmnopqrstuvxyz", i, j;
+      for (i = 0; i < str.length; i++){
+        for (j = 0; j < str.length; j++){
+          delete window[str[i]+str[j]];
+        }
       }
     }
-  }
-},
+  });
+  PerfJS.addSuite(suite);
+})();
 
-
-{
-  name: "625 additional global variables",
-  test: function(){
-    ll ++; 
-  },
-  setUp: function(){
-    var str = "abcdefghijklmnopqrstuvxyz", i, j;
-    for (i = 0; i < str.length; i++){
-      for (j = 0; j < str.length; j++){
-        window[str[i]+str[j]] = 42;
-      }
-    }
-  },
-
-  tearDown: function(){
-    var str = "abcdefghijklmnopqrstuvxyz", i, j;
-    for (i = 0; i < str.length; i++){
-      for (j = 0; j < str.length; j++){
-        delete window[str[i]+str[j]];
-      }
-    }
-  }
-}
-])
+//64 lines before refector
